8a4b27eedbeea9fea875d959c1647f55ef42678e
miljanpavlovic <pavl.miljan@gmail.com>
2015-06-25 23:54:39 +0200

    added volley and some manager classes

---
+++ app/src/main/java/com/transapp/shipper/managers/DbManager.java
@@ -0,0 +1,36 @@
+package com.transapp.shipper.managers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.transapp.shipper.db.FeedReaderDbHelper;
+
+/**
+ * Created by Miljan on 6/25/2015.
+ */
+public class DbManager {
+
+    private static final String TAG = "DbManager";
+
+    private SQLiteDatabase db;
+    private FeedReaderDbHelper mDbHelper;
+    private static DbManager dbManager;
+
+    private DbManager(Context context) {
+        mDbHelper = new FeedReaderDbHelper(context);
+        db = mDbHelper.getWritableDatabase();
+    }
+
+    public static DbManager getInstance(Context context) {
+
+        if (dbManager == null) {
+            dbManager = new DbManager(context);
+        }
+        return dbManager;
+    }
+
+    public SQLiteOpenHelper getDbHelper() {
+        return mDbHelper;
+    }
+}
