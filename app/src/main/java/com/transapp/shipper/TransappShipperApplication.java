696ac0ec8ccdf61b0081bf139ee8db135c6d47e3
Miljan Pavlovic <pavl.miljan@gmail.com>
2015-06-27 16:58:13 +0200

    added signature widget

---
+++ app/src/main/java/com/transapp/shipper/TransappShipperApplication.java
@@ -0,0 +1,16 @@
+package com.transapp.shipper;
+
+import android.app.Application;
+
+/**
+ * Created by Miljan on 6/27/2015.
+ */
+public class TransappShipperApplication extends Application {
+
+    private static final String TAG = "TransappShipperApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
